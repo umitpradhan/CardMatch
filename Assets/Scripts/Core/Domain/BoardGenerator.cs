a5b891c06e897a0de1fd299140d666d9a5136aa1
Umit Pradhan <umitpradhan@gmail.com>
2026-02-28 16:14:26 +0530

    Update BoardGenerator.cs

--- Assets/Scripts/Core/Domain/BoardGenerator.cs
+++ Assets/Scripts/Core/Domain/BoardGenerator.cs
@@ -1,18 +1,40 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class BoardGenerator : MonoBehaviour
+namespace CardMatch.Core.Domain
 {
-    // Start is called before the first frame update
-    void Start()
+    public sealed class BoardGenerator
     {
-        
-    }
+        public IEnumerable<CardModel> Generate(int rows, int columns, int seed)
+        {
+            int totalCards = rows * columns;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if (totalCards % 2 != 0)
+                throw new ArgumentException("Board must contain even number of cards.");
+
+            int pairCount = totalCards / 2;
+
+            var rng = new Random(seed);
+            var pairIds = new List<int>(totalCards);
+
+            // Create pair ids
+            for (int i = 0; i < pairCount; i++)
+            {
+                pairIds.Add(i);
+                pairIds.Add(i);
+            }
+
+            // Fisher-Yates shuffle
+            for (int i = pairIds.Count - 1; i > 0; i--)
+            {
+                int j = rng.Next(i + 1);
+                (pairIds[i], pairIds[j]) = (pairIds[j], pairIds[i]);
+            }
+
+            for (int i = 0; i < totalCards; i++)
+            {
+                yield return new CardModel(i, pairIds[i]);
+            }
+        }
     }
 }
